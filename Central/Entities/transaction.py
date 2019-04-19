d4b5cffe52349ca9edf5a2c23be1cb05b7e4e6b1
reisaolucas@gmail.com <reisaolucas@gmail.com>
2019-04-19 19:54:44 -0300

    changed cpf to idn

--- Central/Entities/transaction.py
+++ Central/Entities/transaction.py
@@ -1,6 +1,6 @@
 class Transaction:
-    def __init__ (self, senderCpf, receiverCpf, valueTransfered, senderSignature):
-        self.senderCpf = senderCpf
-        self.receiverCpf = receiverCpf
+    def __init__ (self, senderIDN, receiverIDN, valueTransfered, senderSignature):
+        self.senderIDN = senderIDN
+        self.receiverIDN = receiverIDN
         self.valueTransfered = valueTransfered
         self.senderSignature = senderSignature
