172dc8665da742ad54ff852a36ae37b0f74ef28f
reisaolucas@gmail.com <reisaolucas@gmail.com>
2019-09-09 20:46:10 -0300

    deleting duplicates

--- Central/Entities/transaction.py
+++
@@ -1,6 +0,0 @@
-class Transaction:
-    def __init__ (self, sender_idn, receiver_idn, value_transfered, sender_signature):
-        self.sender_idn = sender_idn
-        self.receiver_idn = receiver_idn
-        self.value_transfered = value_transfered
-        self.sender_signature = sender_signature
