f12da2c2a435b55d867842f8da17c087e9d1eec2
reisaolucas@gmail.com <reisaolucas@gmail.com>
2019-09-11 02:05:59 -0300

    checking if has balance to transfer value

--- Central/Entities/Transaction.py
+++ Central/Entities/Transaction.py
@@ -1,6 +1,20 @@
+from py_linq import Enumerable
+
 class Transaction:
-    def __init__ (self, sender_idn, receiver_idn, value_transfered, sender_signature):
-        self.sender_idn = sender_idn
-        self.receiver_idn = receiver_idn
-        self.value_transfered = value_transfered
-        self.sender_signature = sender_signature
+    # def __init__ (self, sender_idn, receiver_idn, value_transfered, sender_signature):
+    #     self.sender_idn = sender_idn
+    #     self.receiver_idn = receiver_idn
+    #     self.value_transfered = value_transfered
+    #     self.sender_signature = sender_signature
+
+    def __init__ (self, tx_string: str):
+        self.tx_string = tx_string
+
+    def get_sender_idn (self):
+        return self.tx_string.split(",")[0]
+    
+    def get_value_transfered (self):
+        return float(self.tx_string.split(",")[1])
+
+    def get_receiver_idn (self):
+        return self.tx_string.split(",")[2]
--- Central/PushTransactionToNextBlock.py
+++ Central/PushTransactionToNextBlock.py
@@ -1,13 +1,18 @@
 import json
 from py_linq import Enumerable
-
+from .CentralCore import get_last_block_number
+from py_linq import Enumerable
+from Central.Entities.Transaction import Transaction
 import sys
 sys.path.append("..")
 from User.Entities.Account import AccountList as AccountList
 
 def push(from_idn: str, to_idn: str, amount: float):
-    if check_if_valid_account(from_idn) and check_if_valid_account(to_idn):
-        print ('valid accounts')
+    if check_if_valid_account(from_idn) and \
+        check_if_valid_account(to_idn) and \
+        check_if_has_balance(from_idn, amount):
+        
+        print ('Transfer is valid')
 
 def check_if_valid_account(idn:str):
     try:
@@ -18,11 +23,29 @@ def check_if_valid_account(idn:str):
         raise
     else:
         if Enumerable(accounts_file_obj['account_list']) \
-            .any(lambda x: x['idn'] == idn):
+            .any(lambda x: x['idn'] == idn): 
             return True
         else: 
             raise Exception("Account '{}' does not exist.".format(idn))
     
 
-def check_if_has_balance(idn: str, amount: float):
-    return True
+def check_if_has_balance(sender_idn: str, amount_transfered: float):
+    last_block_number = get_last_block_number()
+    current_balance=0
+    for i in range(last_block_number+1):
+        block_file = open('Central/Databases/Blocks/block{}.json'.format(i), 'r')
+        block_file_data = block_file.read()
+        block_file.close()
+        block_data_object = json.loads(block_file_data)
+        if block_data_object['init_destination'] == sender_idn: 
+            current_balance+=block_data_object['init_value']
+        for tx in block_data_object['tx_dataset']:
+            if Transaction(tx).get_sender_idn() == sender_idn:
+                current_balance-=Transaction(tx).get_value_transfered()
+            if Transaction(tx).get_receiver_idn() == sender_idn:
+                current_balance+=Transaction(tx).get_value_transfered()
+    # check also in the block being added
+    if current_balance>=amount_transfered:
+        return True
+    else: 
+        raise Exception("Sender '{}' does not have balance for this transaction.".format(sender_idn))
